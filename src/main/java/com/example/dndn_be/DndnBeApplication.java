cb321771f3d2fb3bb297cf2c638ff7f9999c9adb
박태수 <taesupark0504@gmail.com>
2025-11-05 23:07:31 +0900

    chore: 기본 세팅

---
+++ src/main/java/com/example/dndn_be/DndnBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dndn_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndnBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DndnBeApplication.class, args);
+    }
+
+}
