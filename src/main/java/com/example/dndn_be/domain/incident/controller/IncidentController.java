5f0a6e4407e42c866951e43b860c3f65bcf5dce0
박태수 <taesupark0504@gmail.com>
2025-11-06 11:18:16 +0900

    feat: incidentController

---
+++ src/main/java/com/example/dndn_be/domain/incident/controller/IncidentController.java
@@ -0,0 +1,11 @@
+package com.example.dndn_be.domain.incident.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/incident")
+@RequiredArgsConstructor
+public class IncidentController {
+}
