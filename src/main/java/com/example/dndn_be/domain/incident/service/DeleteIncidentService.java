6414ea3d2c97e81cb58d6d5dbfa43c0b0220d61e
박태수 <taesupark0504@gmail.com>
2025-11-06 11:49:04 +0900

    feat: 사건 삭제

---
+++ src/main/java/com/example/dndn_be/domain/incident/service/DeleteIncidentService.java
@@ -0,0 +1,27 @@
+package com.example.dndn_be.domain.incident.service;
+
+import com.example.dndn_be.domain.incident.domain.Incident;
+import com.example.dndn_be.domain.incident.domain.repository.IncidentRepository;
+import com.example.dndn_be.domain.incident.exception.IncidentNotFoundException;
+import com.example.dndn_be.domain.user.domain.User;
+import com.example.dndn_be.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteIncidentService {
+    private final IncidentRepository incidentRepository;
+    private final UserFacade userFacade;
+
+    @Transactional
+    public void execute(Long incidentId) {
+        User user = userFacade.getCurrentUser();
+
+        Incident incident = incidentRepository.findById(incidentId)
+                .orElseThrow(() -> IncidentNotFoundException.EXCEPTION);
+
+        incidentRepository.delete(incident);
+    }
+}
