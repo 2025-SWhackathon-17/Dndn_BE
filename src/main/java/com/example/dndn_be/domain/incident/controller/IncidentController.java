81911c12c6ad4b91dc944f827dcf4927b4ee8bc5
박태수 <taesupark0504@gmail.com>
2025-11-06 11:49:19 +0900

    feat: api 모음

--- src/main/java/com/example/dndn_be/domain/incident/controller/IncidentController.java
+++ src/main/java/com/example/dndn_be/domain/incident/controller/IncidentController.java
@@ -2,12 +2,13 @@ package com.example.dndn_be.domain.incident.controller;
 
 import com.example.dndn_be.domain.incident.dto.request.IncidentRequest;
 import com.example.dndn_be.domain.incident.dto.response.IncidentResponse;
-import com.example.dndn_be.domain.incident.service.CreateIncidentService;
-import com.example.dndn_be.domain.incident.service.QueryIncidentDetailService;
+import com.example.dndn_be.domain.incident.service.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/incident")
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.*;
 public class IncidentController {
     private final CreateIncidentService createIncidentService;
     private final QueryIncidentDetailService queryIncidentDetailService;
+    private final QueryAllIncidentService queryAllIncidentService;
+    private final UpdateIncidentService updateIncidentService;
+    private final DeleteIncidentService deleteIncidentService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -26,5 +30,23 @@ public class IncidentController {
     @ResponseStatus(HttpStatus.OK)
     public IncidentResponse queryIncident(@PathVariable("incident-id") Long incidentId) {
         return queryIncidentDetailService.execute(incidentId);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<IncidentResponse> queryAllIncident() {
+        return queryAllIncidentService.execute();
+    }
+
+    @PatchMapping("/{incident-id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateIncident(@PathVariable("incident-id") Long incidentId, @RequestBody @Valid IncidentRequest request) {
+        updateIncidentService.execute(incidentId, request);
+    }
+
+    @DeleteMapping("/{incident-id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteIncident(@PathVariable("incident-id") Long incidentId) {
+        deleteIncidentService.execute(incidentId);
     }
 }
